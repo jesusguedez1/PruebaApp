6b3d2b9c6bd4f9dcd67d5179ec49004200171f21
jesusguedez1 <jesus.gimenez1799@gmail.com>
2024-04-08 13:00:57 -0500

    primer comit

---
+++ index.js
@@ -0,0 +1,49 @@
+//Variables.........
+
+//carrito de compras
+let mBurguer = document.querySelector("#menuHamburguesa");
+let carShopping = document.querySelector(".principalProduct");
+let exitCarShopping = document.querySelector("#menuHamburguesa2");
+let enviarPedido = document.querySelector("#pedir");
+
+//agregar al carrito
+let btn = document.querySelectorAll(".btn-compras");
+let datosAgregados = document.querySelector("#productos");
+let namee = document.querySelector("#nombre");
+let precio =document.querySelector("#precio");
+let lorem = document.querySelector("#lorem");
+let total = document.querySelector("#total");
+
+let precioTotal = 0;
+
+let agregarDatos = (event) =>{
+    let nombreProducto = namee.textContent;
+    let precioProducto = parseFloat(precio.textContent);
+
+    let agregarCompra = document.createElement (`h4`);
+    agregarCompra.textContent = nombreProducto + " " + precioProducto;
+    datosAgregados.appendChild(agregarCompra);
+
+    precioTotal += precioProducto;
+    total.textContent = "Total: " + precioTotal + " S/";
+
+    lorem.style.display = "none";
+}
+
+exitCarShopping.addEventListener("click",  exitcar = () =>{
+    carShopping.style.display = "none";
+    mBurguer.style.display = "flex";
+    exitCarShopping.style.display = "none"}
+    );
+
+mBurguer.addEventListener("click", menuHover = () =>{
+    carShopping.style.display = "flex";
+    mBurguer.style.display = "none";
+    exitCarShopping.style.display = "flex";
+    });
+
+btn.forEach(button => {
+    button.addEventListener("click", agregarDatos);
+});
+
+
